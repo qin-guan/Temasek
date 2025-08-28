7a7a539c96521ac7313de85fa930993d451611e1
qinguan <qinguan20040914@gmail.com>
2025-08-28 17:59:15 +0800

    Update

--- Temasek.Auth/Options/FormGovSgOptions.cs
+++ Temasek.Auth/Options/FormGovSgOptions.cs
@@ -3,6 +3,4 @@ namespace Temasek.Auth.Options;
 public class FormGovSgOptions
 {
     public required string? CallbackApiKey { get; set; }
-    public required string? FormId { get; set; }
-    public required string? FormSessionFieldId { get; set; }
 }
