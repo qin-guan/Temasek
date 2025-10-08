e64e95d64dd6fb670729ad8ad8b6b88193638b3d
Qin Guan <qinguan20040914@gmail.com>
2025-10-08 11:52:55 +0800

    Tire

--- Temasek.AppHost/AppHost.cs
+++ Temasek.AppHost/AppHost.cs
@@ -1,5 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddProject<Projects.Temasek_Operatorr>("temasek-operatorr");
+
 builder.AddProject<Projects.Temasek_Auth>("temasek-auth");
 
 builder.AddProject<Projects.Temasek_Calendarr>("temasek-calendarr");
