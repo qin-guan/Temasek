bcf22c073043cc62a25cc20dab9967c702d2aa1f
Qin Guan <qinguan20040914@gmail.com>
2025-11-09 21:55:48 +0800

    Tired

--- Temasek.AppHost/AppHost.cs
+++ Temasek.AppHost/AppHost.cs
@@ -1,8 +1,18 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Temasek_Operatorr>("temasek-operatorr");
+var auth = builder.AddProject<Projects.Temasek_Auth>("temasek-auth");
+builder.AddNpmApp("temasek-auth-client", "../Temasek.Auth//Temasek.Auth.Client", "dev")
+    .WithReference(auth)
+    .WaitFor(auth)
+    .WithHttpEndpoint(env:"PORT")
+    .WithExternalHttpEndpoints();
 
-builder.AddProject<Projects.Temasek_Auth>("temasek-auth");
+var operatorr = builder.AddProject<Projects.Temasek_Operatorr>("temasek-operatorr");
+builder.AddNpmApp("temasek-operatorr-client", "../Temasek.Operatorr/Temasek.Operatorr.Client", "dev")
+    .WithReference(operatorr)
+    .WaitFor(operatorr)
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints();
 
 builder.AddProject<Projects.Temasek_Calendarr>("temasek-calendarr");
 
